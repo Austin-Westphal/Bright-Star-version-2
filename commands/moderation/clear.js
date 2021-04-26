022ca0c27fd0ad82d5a20195fab73f6aa829d06e
Austin Westphal <78154173+Austin-Westphal@users.noreply.github.com>
2021-04-26 09:49:24 -0400

    Add files via upload

---
+++ commands/moderation/clear.js
@@ -0,0 +1,32 @@
+module.exports = {
+  name: "purge",
+  aliases:[],
+  category: "moderation",
+  description: "Delete bulk messages with 1 command",
+  run: async (client, message, args) => {
+    
+            
+    if (message.deletable) {
+        message.delete();
+    }
+
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+        return message.reply("Missing Permissions!").then(m => m.delete(5000));
+    }
+
+    if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+        return message.reply("This is not a number").then(m => m.delete(5000));
+    }
+
+    let deleteAmount;
+    if (parseInt(args[0]) > 1000) {
+        deleteAmount = 1000;
+    } else {
+        deleteAmount = parseInt(args[0]);
+    }
+
+    message.channel.bulkDelete(deleteAmount, true)
+    .catch(err => message.reply(`Something went wrong... ${err}`));
+    
+  }
+}
