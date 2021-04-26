888c96364a512ffc9605c9ffe041eeb000282919
Austin Westphal <78154173+Austin-Westphal@users.noreply.github.com>
2021-04-26 09:47:22 -0400

    Add files via upload

---
+++ commands/fun/reverse.js
@@ -0,0 +1,14 @@
+const Discord = module.require("discord.js");
+
+module.exports = {
+    name: "reverse",
+    category: "fun",
+    description: "Reverse the text entered",
+    run: async(client, message, args) => {
+       let str = args.join(" ");
+       if (!str) {
+       return message.channel.send("Enter some text to be reversed")
+       }
+       message.channel.send(str.split('').reverse().join(''));
+    }
+}
