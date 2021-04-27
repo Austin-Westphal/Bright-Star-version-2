82fdc15289e726fc2e14b784bd6aa7a25e4ce212
Austin Westphal <78154173+Austin-Westphal@users.noreply.github.com>
2021-04-27 09:19:19 -0400

    Update quarterly.js

--- commands/economy/quarterly.js
+++ commands/economy/quarterly.js
@@ -1,39 +1,38 @@
 const { MessageEmbed } = require("discord.js");
-const config = require('../../config.json');
 const db = require("quick.db");
 const ms = require("parse-ms");
 
 module.exports = {
-    
+   
         name: "quarterly",
         aliases: ["quart"],
         category: "economy",
-        description: "Gives You 1,750 per 3 months",
-        usage: "+quarterly",
+        description: "Gives You 10000 each 3 months",
+        usage: "quarterly | quart",
         accessableby: "everyone"
-  ,
+    ,
     run: async (bot, message, args) => {
+
         let user = message.author;
-
         let timeout = 7889400000;
-        let amount = 1750;
+        let amount = 10000;
 
-        let quarterly = await db.fetch(`quarterly_${user.id}`);
+        let monthly = await db.fetch(`monthly_${user.id}`);
 
-        if (daily !== null && timeout - (Date.now() - quarterly) > 0) {
-            let time = ms(timeout - (Date.now() - quarterly));
+        if (monthly !== null && timeout - (Date.now() - monthly) > 0) {
+            let time = ms(timeout - (Date.now() - monthly));
 
             let timeEmbed = new MessageEmbed()
                 .setColor("GREEN")
-                .setDescription(`❌ You've already collected your quarterly reward\n\nCollect it again in ${time.hours}h ${time.minutes}m ${time.seconds}s `);
+                .setDescription(`❌ You have already collected your quarterly reward\n\nCollect it again in ${time.days}d ${time.hours}h ${time.minutes}m ${time.seconds}s `);
             message.channel.send(timeEmbed)
         } else {
             let moneyEmbed = new MessageEmbed()
                 .setColor("GREEN")
-                .setDescription(`✅ You've collected your quarterly reward of ${amount} coins`);
+                .setDescription(`✅ You've collected your quarterly reward of ${amount} coins`); 
             message.channel.send(moneyEmbed)
             db.add(`money_${user.id}`, amount)
-            db.set(`quarterly_${user.id}`, Date.now())
+            db.set(`monthly_${user.id}`, Date.now())
 
 
         }
