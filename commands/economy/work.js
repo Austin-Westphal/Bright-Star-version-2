2968b8645deb97eeb8a0548bfdc3ddaae57536fa
Austin Westphal <78154173+Austin-Westphal@users.noreply.github.com>
2021-04-27 10:36:58 -0400

    Update work.js

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -1,11 +1,42 @@
-module.exports = ({
-    name: "work",
-    desription: "Work to earn more money!",
-    usage: "",
-    category: "economy",
-    code: `$color[RANDOM]
-    $description[You worked and got $random[50;2000]💴!]
-    $setGlobalUserVar[money;$sum[$getGlobalUserVar[money];$random[50;2000]]]
-    $cooldown[1h;{description: You can work again in **%time%**!}{color:RANDOM}]
-    $onlyIf[$checkContains[$channelType;text;news]==true;]`    
-})
+const Discord = require('discord.js');
+const ms = require("parse-ms");
+
+module.exports = {
+		name: "work",
+		description: "Work to get some money!",
+		category: "economy",
+run: async (client, message, args) => {
+
+    let user = message.author;
+    
+    let author = await client.db.fetch(`work_${message.guild.id}_${user.id}`)
+
+    let timeout = 300000;
+    
+    if (author !== null && timeout - (Date.now() - author) > 0) {
+        let time = ms(timeout - (Date.now() - author));
+    
+        let timeEmbed = new Discord.MessageEmbed()
+        .setColor("#FFFFFF")
+        .setDescription(`<a:false:737764891657633814> You have already worked recently\n\nTry again in ${time.minutes}m ${time.seconds}s `);
+        message.channel.send(timeEmbed)
+      } else {
+
+        let replies = ['Programmer','Builder','Waiter','Busboy','Chief','Mechanic', 'Teacher', 'Governor']
+
+        let result = Math.floor((Math.random() * replies.length));
+        let amounta = Math.floor(Math.random() * 80) + 1;
+        let multiplier = await client.db.fetch(`multiplier_${message.guild.id}`);
+        if(!multiplier) multiplier = 1;
+        let amount =  amounta * multiplier;
+
+        let embed1 = new Discord.MessageEmbed()
+        .setColor("#FFFFFF")
+        .setDescription(`<a:yes:739569362440159252> You worked as a ${replies[result]} and earned ${amount} coins`);
+        message.channel.send(embed1)
+        
+        await client.db.add(`money_${message.guild.id}_${user.id}.pocket`, amount)
+        await client.db.set(`work_${message.guild.id}_${user.id}`, Date.now())
+		};
+	}
+}
